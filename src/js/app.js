e176f77c526bc4490be05a3cd1eb6cbadb9a6eba
ewgenij87snwork <ewgenij87snwork@gmail.com>
2018-12-03 14:41:51 +0100

    add preloader

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,17 @@
 import sayHello from './lib/sayHello.js';
 
 sayHello();
+
+// Script for preloader
+// In html tag "body" have id='loading'
+// For understand how time need -- must test in crossbrowser (chrome extention, network)
+
+document.addEventListener('DOMContentLoaded', function() {
+  toggleMenu();
+  setTimeout(function() {
+    'use strict';
+    var body = document.getElementById('loading');
+    body.classList.add('loaded');
+  }, 1000);
+});
+
